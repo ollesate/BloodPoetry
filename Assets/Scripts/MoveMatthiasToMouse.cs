7796b255bed2779b72eed3e2467c82b30ff7c2a4
theyuppi <perjonaserik@gmail.com>
2016-01-30 11:54:27 +0100

    Matthias arrived
    The brave wanderer has arrived and is looking for trouble...

---
+++ Assets/Scripts/MoveMatthiasToMouse.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveMatthiasToMouse : MonoBehaviour {
+
+	Vector3 startPos;
+
+	void Start () {
+		startPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton(0) || Input.GetMouseButton(1) )
+		{
+			//Vector3 pos = Input.mousePosition;
+			//pos.z = 0;
+			//transform.position = pos;
+
+			transform.position = startPos;
+			Quaternion rot = Random.rotation;
+			rot.x = 0;
+			rot.y = 0;
+			rot.z = 0;
+			transform.rotation = rot;
+		}
+	
+	}
+}
