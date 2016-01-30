b718399666a9dc996c271e45f62f580605634177
Martin Grönlund <martingronlund@live.se>
2016-01-30 03:34:50 +0100

    Test
    Signed-off-by: Martin Grönlund <martingronlund@live.se>

---
+++ Scripts/DayCycle.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DayCycle : MonoBehaviour {
+
+    public int rotationOffset_deg;
+    public int minutesPerDay;
+
+    float rotation;
+    float rotationSpeed;
+
+    // Use this for initialization
+    void Start() {
+        transform.eulerAngles = new Vector3( -rotationOffset_deg, 90, 0 );
+        float rotationDistance = rotationOffset_deg * 2 + 180;
+        rotationSpeed = (rotationDistance * Mathf.PI / 180) / (minutesPerDay * 60);
+	}
+
+    // Update is called once per frame
+    void Update() {
+        transform.Rotate(new Vector3(rotationSpeed, 0, 0));
+	}
+}
