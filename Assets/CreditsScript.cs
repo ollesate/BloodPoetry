88dfc0aac5a28af9b239b1fed690aaac1d9bfef2
theyuppi <perjonaserik@gmail.com>
2016-01-30 10:44:46 +0100

    MenuSystem
    Halfarsed job by a wholearsed man. Cheeky?

---
+++ Assets/CreditsScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsScript : MonoBehaviour {
+
+	public GameObject[] MenuItems;
+	
+
+	void Update () {
+		if (Input.GetButton("Fire2"))
+		{
+				MenuItems[0].SetActive(true);
+				MenuItems[1].SetActive(false);
+		}
+	
+	}
+}
