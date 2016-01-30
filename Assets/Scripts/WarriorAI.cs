3212837963134c0f290f8f97d777664614a71e60
Olof Sjöholm <olof.sjoholm@gmail.com>
2016-01-30 16:01:44 +0100

    Added three warrior types
    Blowgun warrior, Spear warrior and Club warrior

--- Assets/Scripts/WarriorAI.cs
+++ Assets/Scripts/WarriorAI.cs
@@ -1,16 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(Health))]
-public class WarriorAI : MonoBehaviour {
+public class WarriorAI : MonoBehaviour
+{
 
     public float MovementSpeed = .5f;
     public float AttackDelay = 1.0f;
     public float AttackDamage = 1.0f;
-    public float DeathDelay = .5f;
+    public float DeathDelay = .25f;
     public GameObject target;
     public float attackCooldown;
+    public Warrior warrior;
+    public Warrior.WarriorType WarriorType;
 
     private float mScaleX;
     private Rigidbody2D mRigidbody;
@@ -18,21 +22,36 @@ public class WarriorAI : MonoBehaviour {
     private WarriorAttackDetector mAttackBox;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         mHealth = GetComponent<Health>();
         mHealth.DeathDelay = DeathDelay;
         mRigidbody = GetComponent<Rigidbody2D>();
         mScaleX = transform.localScale.x;
+
+        if (WarriorType == Warrior.WarriorType.BLOWGUN)
+        {
+            warrior = new BlowGunWarrior();
+        }
+        else if (WarriorType == Warrior.WarriorType.CLUB)
+        {
+            warrior = new ClubWarrior();
+        }
+        else if (WarriorType == Warrior.WarriorType.SPEAR)
+        {
+            warrior = new SpearWarrior();
+        }
     }
 
-    void Awake ()
+    void Awake()
     {
         mAttackBox = GetComponentInChildren<WarriorAttackDetector>();
         mAttackBox.OnTriggerListener += (other) => enemyDetected(other);
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         if (target != null)
         {
             attack();
@@ -43,12 +62,13 @@ public class WarriorAI : MonoBehaviour {
         }
     }
 
-    void move ()
+    void move()
     {
-        if(MovementSpeed > 0)
+        if (MovementSpeed > 0)
         {
             transform.localScale = new Vector3(mScaleX, transform.localScale.y, transform.localScale.z);
-        } else
+        }
+        else
         {
             transform.localScale = new Vector3(-mScaleX, transform.localScale.y, transform.localScale.z);
         }
@@ -60,15 +80,97 @@ public class WarriorAI : MonoBehaviour {
         attackCooldown -= Time.deltaTime;
         if (attackCooldown <= 0)
         {
-            Debug.Log("Warrior attacked another warrior for " + AttackDamage);
-            target.GetComponent<Health>().TakeDamage(AttackDamage);
+            float damage = getModifiedDamage(target.gameObject.GetComponent<WarriorAI>().warrior);
+            Debug.Log("Warrior " + warrior.warriorType.ToString() + " attacked another warrior for " + damage);
+            target.GetComponent<Health>().TakeDamage(damage);
             attackCooldown = AttackDelay;
         }
     }
 
     void enemyDetected(Collider2D other)
     {
-        Debug.Log("Warrior detected another warrior");
         target = other.gameObject;
+        Debug.Log(warrior.warriorType.ToString() + " Warrior detected a " + target.GetComponent<WarriorAI>().WarriorType.ToString() + " Warrior");
+        
+    }
+
+    float getModifiedDamage(Warrior other)
+    {
+        if (warrior.StrongAgainst() == other.warriorType)
+        {
+            return AttackDamage * 2;
+        }
+
+        return AttackDamage;
+    }
+
+}
+
+public abstract class Warrior
+{
+
+    public enum WarriorType
+    {
+        CLUB,
+        SPEAR,
+        BLOWGUN
+    }
+    public WarriorType warriorType;
+    public abstract WarriorType WeakAgainst();
+    public abstract WarriorType StrongAgainst();
+
+}
+
+public class ClubWarrior : Warrior
+{
+    public ClubWarrior()
+    {
+        warriorType = WarriorType.CLUB;
+    }
+
+    public override WarriorType StrongAgainst()
+    {
+        return WarriorType.BLOWGUN;
+    }
+
+    public override WarriorType WeakAgainst()
+    {
+        return WarriorType.SPEAR;
+    }
+}
+
+public class SpearWarrior : Warrior
+{
+    public SpearWarrior()
+    {
+        warriorType = WarriorType.SPEAR;
+    }
+
+    public override WarriorType StrongAgainst()
+    {
+        return WarriorType.CLUB;
+    }
+
+    public override WarriorType WeakAgainst()
+    {
+        return WarriorType.BLOWGUN;
+    }
+}
+
+public class BlowGunWarrior : Warrior
+{
+    public BlowGunWarrior()
+    {
+        warriorType = WarriorType.BLOWGUN;
+    }
+
+    public override WarriorType StrongAgainst()
+    {
+        return WarriorType.SPEAR;
+    }
+
+    public override WarriorType WeakAgainst()
+    {
+        return WarriorType.CLUB;
     }
 }
