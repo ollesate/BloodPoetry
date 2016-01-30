792b42391f53485471303c14448c45297c406621
Olof Sjöholm <olof.sjoholm@gmail.com>
2016-01-30 14:56:36 +0100

    Added initial warrior AI

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour
+{
+
+    public float StartHealth = 3.0f;
+    public float DeathDelay = 0.0f;
+
+    public float currentHealth;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentHealth = StartHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+        if(currentHealth <= 0)
+        {
+            GameObject.Destroy(gameObject, DeathDelay);
+        }
+    }
+}
