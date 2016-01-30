9d9e3b1650a3c0f242ded276bec581be65fd916a
Olof Sjöholm <olof.sjoholm@gmail.com>
2016-01-30 13:42:54 +0100

    Added first initial Warrior AI

---
+++ Assets/Scripts/WarriorAttackDetector.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class WarriorAttackDetector : MonoBehaviour
+{
+
+    public delegate void EventHandler(Collider2D other);
+    public event EventHandler OnTriggerListener;
+
+    private BoxCollider2D mTriggerBox;
+    // Use this for initialization
+    void Start()
+    {
+        mTriggerBox = GetComponent<BoxCollider2D>();
+        mTriggerBox.isTrigger = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Warrior") OnTriggerListener(other);
+    }
+}
