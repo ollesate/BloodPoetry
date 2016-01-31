0bb567fbed9e1684f852736b5b04367923a599cc
theyuppi <perjonaserik@gmail.com>
2016-01-31 11:36:19 +0100

    GLORY TO HYPNOTOAD
    or you'll get thrown from the pyramide :+1:

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,255 +2,258 @@
 
 public class Player : MonoBehaviour {
 
-    /* Enums */
-    public enum State
-    {
-        Idle,
-        ChoosingFate,
-            ChoosingSoldierFate,
-                RecruitingBlowgunner,
-                RecruitingClubman,
-                RecruitingSpearman,
-            ChoosingDivineFate,
-                SacrificingForFertility,
-                SacrificingForFun,
-                SacrificingForPower,
-        Throwing,
-        NUM
-    }
-
-    /* Fields */
-    public bool isHuman;
-    public int playerIndex;
-    public Choicemaker choicemaker;
-
-    ProperInput input;
-    State state;
-    SacrificeThrow throwing;
-    private QueueSystem queueSystem;
-    private ArmySpawner armySpawner;
-    private DivineBuffs divineBuffs;
-
-    /* Lifetime Methods */
-    void Start () {
-        input = new ProperInput( playerIndex );
-        state = State.Idle;
-        throwing = GetComponentInChildren<SacrificeThrow>();
-        armySpawner = GetComponentInChildren<ArmySpawner>();
-        queueSystem = GetComponentInChildren<QueueSystem>();
-        divineBuffs = GetComponent<DivineBuffs>();
-        if(queueSystem == null)
-        {
-            Debug.Log("No Queuesystem is added to pyramid");
-        }
-        if (armySpawner == null)
-        {
-            Debug.Log("Armyspawner is null");
-        }
+	/* Enums */
+	public enum State
+	{
+		Idle,
+		ChoosingFate,
+			ChoosingSoldierFate,
+				RecruitingBlowgunner,
+				RecruitingClubman,
+				RecruitingSpearman,
+			ChoosingDivineFate,
+				SacrificingForFertility,
+				SacrificingForFun,
+				SacrificingForPower,
+		Throwing,
+		NUM
 	}
 
-    void Update() {
+	/* Fields */
+	public bool isHuman;
+	public int playerIndex;
+	public Choicemaker choicemaker;
+	public GameObject myPriest;
 
-        // Update queue.
+	ProperInput input;
+	State state;
+	SacrificeThrow throwing;
+	private QueueSystem queueSystem;
+	private ArmySpawner armySpawner;
+	private DivineBuffs divineBuffs;
 
+	/* Lifetime Methods */
+	void Start () {
+		input = new ProperInput( playerIndex );
+		state = State.Idle;
+		throwing = GetComponentInChildren<SacrificeThrow>();
+		armySpawner = GetComponentInChildren<ArmySpawner>();
+		queueSystem = GetComponentInChildren<QueueSystem>();
+		divineBuffs = GetComponent<DivineBuffs>();
+		if(queueSystem == null)
+		{
+			Debug.Log("No Queuesystem is added to pyramid");
+		}
+		if (armySpawner == null)
+		{
+			Debug.Log("Armyspawner is null");
+		}
+	}
 
-        // Update state.
-        Debug.Log( state );
-        switch ( state )
-        {
-            case State.Idle:
-                {
-                    if (queueSystem.CanSacrificeVillager())
-                    {
-                        state = State.ChoosingFate;
-                    }
-                    // Check queue for people.
-                    
-                }
-                break;
-            case State.ChoosingFate:
-                {
-                    // Choose fate
-                    if (!choicemaker.isUsing)
-                        choicemaker.StartUsing();
-                    
-                    Choicemaker.Choice choice = choicemaker.Poll();
-                    if (choice != Choicemaker.Choice.None)
-                    {
-                        switch (choice)
-                        {
-                            case Choicemaker.Choice.Blue:
-                                state = State.ChoosingSoldierFate;
-                                break;
-                            case Choicemaker.Choice.Green:
-                                state = State.Throwing;
-                                break;
-                            case Choicemaker.Choice.Red:
-                                state = State.ChoosingDivineFate;
-                                break;
-                        }
-                    }
-                }
-                break;
-            case State.Throwing:
-                {
-                    if(throwing.isUsing == false)
-                    {
-                        queueSystem.SacrificeVillagerDestroy();
-                        // Start throwing person.
-                        throwing.StartUsing();
-                    }
-                }
-                break;
+	void Update() {
 
-            /* Divine fates */
-            case State.ChoosingDivineFate:
-                {
-                    // Update display of choice-thingy; show sacrifice paths.
-                    if ( !choicemaker.isUsing )
-                        choicemaker.StartUsing();
+		// Update queue.
+		myPriest.GetComponent<Animator>().SetBool("Attack", false);
 
-                    Choicemaker.Choice choice = choicemaker.Poll();
-                    if ( choice != Choicemaker.Choice.None )
-                    {
-                        queueSystem.SacrificeVillagerDestroy();
-                        switch ( choice )
-                        {
-                            case Choicemaker.Choice.Blue:
-                                state = State.SacrificingForPower;
-                                break;
-                            case Choicemaker.Choice.Green:
-                                state = State.SacrificingForFun;
-                                break;
-                            case Choicemaker.Choice.Red:
-                                state = State.SacrificingForFertility;
-                                break;
-                        }
-                    }
-                }
-                break;
-            case State.SacrificingForFertility:
-                {
-                    // Increase City Fertility
-                    divineBuffs.BuffFertility();
-                    state = State.Idle;
-                }
-                break;
-            case State.SacrificingForFun:
-                {
-                    // Maybe do something?
-                    state = State.Idle;
-                }
-                break;
-            case State.SacrificingForPower:
-                {
-                    // Increase Soldier Strength.
-                    divineBuffs.BuffPower();
-                    state = State.Idle;
-                }
-                break;
+		// Update state.
+		Debug.Log( state );
+		switch ( state )
+		{
+			case State.Idle:
+				{
+					if (queueSystem.CanSacrificeVillager())
+					{
+						state = State.ChoosingFate;
+					}
+					// Check queue for people.
+					
+				}
+				break;
+			case State.ChoosingFate:
+				{
+					// Choose fate
+					if (!choicemaker.isUsing)
+						choicemaker.StartUsing();
+					
+					Choicemaker.Choice choice = choicemaker.Poll();
+					if (choice != Choicemaker.Choice.None)
+					{
+						switch (choice)
+						{
+							case Choicemaker.Choice.Blue:
+								state = State.ChoosingSoldierFate;
+								break;
+							case Choicemaker.Choice.Green:
+								state = State.Throwing;
+								break;
+							case Choicemaker.Choice.Red:
+								state = State.ChoosingDivineFate;
+								break;
+						}
+					}
+				}
+				break;
+			case State.Throwing:
+				{
+					if(throwing.isUsing == false)
+					{
+						queueSystem.SacrificeVillagerDestroy();
+						// Start throwing person.
+						throwing.StartUsing();
+					}
+				}
+				break;
 
-                /* Soldier fates */
-            case State.ChoosingSoldierFate:
-                {
-                    // Update display of choice-thingy; show soldier paths.
-                    if ( !choicemaker.isUsing )
-                        choicemaker.StartUsing();
+			/* Divine fates */
+			case State.ChoosingDivineFate:
+				{
+					// Update display of choice-thingy; show sacrifice paths.
+					if (!choicemaker.isUsing)
+					{ 
+						choicemaker.StartUsing();
+					}
+					Choicemaker.Choice choice = choicemaker.Poll();
+					if ( choice != Choicemaker.Choice.None )
+					{
+						queueSystem.SacrificeVillagerDestroy();
+						switch ( choice )
+						{
+							case Choicemaker.Choice.Blue:
+								state = State.SacrificingForPower;
+								break;
+							case Choicemaker.Choice.Green:
+								state = State.SacrificingForFun;
+								break;
+							case Choicemaker.Choice.Red:
+								state = State.SacrificingForFertility;
+								break;
+						}
+						myPriest.GetComponent<Animator>().SetBool("Attack", true); 
+					}
+				}
+				break;
+			case State.SacrificingForFertility:
+				{
+					// Increase City Fertility
+					divineBuffs.BuffFertility();
+					state = State.Idle;
+				}
+				break;
+			case State.SacrificingForFun:
+				{
+					// Maybe do something?
+					state = State.Idle;
+				}
+				break;
+			case State.SacrificingForPower:
+				{
+					// Increase Soldier Strength.
+					divineBuffs.BuffPower();
+					state = State.Idle;
+				}
+				break;
 
-                    Choicemaker.Choice choice = choicemaker.Poll();
-                    if ( choice != Choicemaker.Choice.None )
-                    {
-                        queueSystem.SacrificeVillagerDestroy();
-                        switch ( choice )
-                        {
-                            case Choicemaker.Choice.Blue:
-                                state = State.RecruitingSpearman;
-                                break;
-                            case Choicemaker.Choice.Green:
-                                state = State.RecruitingBlowgunner;
-                                break;
-                            case Choicemaker.Choice.Red:
-                                state = State.RecruitingClubman;
-                                break;
-                        }
-                    }
-                }
-                break;
-            case State.RecruitingBlowgunner:
-                {
-                    // Recruit Blowgunner.
-                    reqruitBlowgunner();
-                    state = State.Idle;
-                }
-                break;
-            case State.RecruitingClubman:
-                {
-                    // Recruit Clubman.
-                    reqruitClubman();
-                    state = State.Idle;
-                }
-                break;
-            case State.RecruitingSpearman:
-                {
-                    // Recruit Spearman.
-                    reqruitSpearman();
-                    state = State.Idle;
-                }
-                break;
-        }
-    }
+				/* Soldier fates */
+			case State.ChoosingSoldierFate:
+				{
+					// Update display of choice-thingy; show soldier paths.
+					if ( !choicemaker.isUsing )
+						choicemaker.StartUsing();
 
-    public void SetState(State state)
-    {
-        this.state = state;
-    }
+					Choicemaker.Choice choice = choicemaker.Poll();
+					if ( choice != Choicemaker.Choice.None )
+					{
+						queueSystem.SacrificeVillagerDestroy();
+						switch ( choice )
+						{
+							case Choicemaker.Choice.Blue:
+								state = State.RecruitingSpearman;
+								break;
+							case Choicemaker.Choice.Green:
+								state = State.RecruitingBlowgunner;
+								break;
+							case Choicemaker.Choice.Red:
+								state = State.RecruitingClubman;
+								break;
+						}
+					}
+				}
+				break;
+			case State.RecruitingBlowgunner:
+				{
+					// Recruit Blowgunner.
+					reqruitBlowgunner();
+					state = State.Idle;
+				}
+				break;
+			case State.RecruitingClubman:
+				{
+					// Recruit Clubman.
+					reqruitClubman();
+					state = State.Idle;
+				}
+				break;
+			case State.RecruitingSpearman:
+				{
+					// Recruit Spearman.
+					reqruitSpearman();
+					state = State.Idle;
+				}
+				break;
+		}
+	}
 
-    private void reqruitSpearman()
-    {
-        if(armySpawner != null)
-        {
-            armySpawner.SpawnUnit(Warrior.WarriorType.SPEAR);
-            Debug.Log("Spawning a spearman");
-        }
-        else
-        {
+	public void SetState(State state)
+	{
+		this.state = state;
+	}
 
-        }
-    }
+	private void reqruitSpearman()
+	{
+		if(armySpawner != null)
+		{
+			armySpawner.SpawnUnit(Warrior.WarriorType.SPEAR);
+			Debug.Log("Spawning a spearman");
+		}
+		else
+		{
 
-    private void reqruitClubman()
-    {
-        if (armySpawner != null)
-        {
-            armySpawner.SpawnUnit(Warrior.WarriorType.CLUB);
-            Debug.Log("Spawning a clubman");
-        }
-        else
-        {
+		}
+	}
 
-        }
-    }
+	private void reqruitClubman()
+	{
+		if (armySpawner != null)
+		{
+			armySpawner.SpawnUnit(Warrior.WarriorType.CLUB);
+			Debug.Log("Spawning a clubman");
+		}
+		else
+		{
 
-    private void reqruitBlowgunner()
-    {
-        if (armySpawner != null)
-        {
-            Debug.Log("Spawning a blowgunner");
-            armySpawner.SpawnUnit(Warrior.WarriorType.BLOWGUN);
-        }
-        else
-        {
+		}
+	}
 
-        }
-    }
-    
-    /* Methods */
-    public IButtonMap GetMap( ButtonAction action ) { return input.GetMap(action); }
-    public float Getf( ButtonAction action ) { return input.Getf(action); }
-    public bool Get( ButtonAction action ) { return input.Get(action); }
-    public bool GetDown( ButtonAction action ) { return input.GetDown(action); }
-    public bool GetUp( ButtonAction action ) { return input.GetUp(action); }
+	private void reqruitBlowgunner()
+	{
+		if (armySpawner != null)
+		{
+			Debug.Log("Spawning a blowgunner");
+			armySpawner.SpawnUnit(Warrior.WarriorType.BLOWGUN);
+		}
+		else
+		{
 
-    public IAxisMap GetMap( AxisAction action ) { return input.GetMap(action); }
-    public float Get( AxisAction action ) { return input.Get(action); }
+		}
+	}
+	
+	/* Methods */
+	public IButtonMap GetMap( ButtonAction action ) { return input.GetMap(action); }
+	public float Getf( ButtonAction action ) { return input.Getf(action); }
+	public bool Get( ButtonAction action ) { return input.Get(action); }
+	public bool GetDown( ButtonAction action ) { return input.GetDown(action); }
+	public bool GetUp( ButtonAction action ) { return input.GetUp(action); }
+
+	public IAxisMap GetMap( AxisAction action ) { return input.GetMap(action); }
+	public float Get( AxisAction action ) { return input.Get(action); }
 }
--- Assets/Scripts/Sacrifices/SacrificeThrow.cs
+++ Assets/Scripts/Sacrifices/SacrificeThrow.cs
@@ -3,73 +3,76 @@ using System.Collections;
 
 public class SacrificeThrow : MonoBehaviour {
 
-    /* Fields */
-    public GameObject villagerPrefab;
-    public GameObject targetSprite;
-    public float loadTime;
-    public float forceFactor;
+	/* Fields */
+	public GameObject villagerPrefab;
+	public GameObject targetSprite;
+	public GameObject myPriest;
+	public float loadTime;
+	public float forceFactor;
 
-    Player player;
-    public bool isUsing;
-    float power_newton;
-    float angle_rad;
-    Vector2 dir;
-    float time;
+	Player player;
+	public bool isUsing;
+	float power_newton;
+	float angle_rad;
+	Vector2 dir;
+	float time;
 
-    // Use this for initialization
-    void Start()
-    {
-        isUsing = false;
-        player = gameObject.GetComponentInParent<Player>();
-    }
+	// Use this for initialization
+	void Start()
+	{
+		isUsing = false;
+		player = gameObject.GetComponentInParent<Player>();
+	}
 
-    public void StartUsing()
-    {
-        isUsing = true;
-        GetComponent<Fading>().FadeIn();
-    }
+	public void StartUsing()
+	{
+		isUsing = true;
+		GetComponent<Fading>().FadeIn();
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (isUsing)
-        {
-            if ( time < 1 )
-            {
-                time += Time.deltaTime / loadTime;
+	// Update is called once per frame
+	void Update()
+	{
+		myPriest.GetComponent<Animator>().SetBool("Throw", false);
+		if (isUsing)
+		{
+			if ( time < 1 )
+			{
+				time += Time.deltaTime / loadTime;
 
-                if ( time > 1 )
-                {
-                    time = 1;
-                }
-            }
+				if ( time > 1 )
+				{
+					time = 1;
+				}
+			}
 
-            if ( player.GetDown( ButtonAction.Green ) )
-            {
-                // Complete
-                GameObject thrown = Instantiate(villagerPrefab);
-                thrown.transform.position = gameObject.transform.position;
+			if ( player.GetDown( ButtonAction.Green ) )
+			{
+				// Complete
+				GameObject thrown = Instantiate(villagerPrefab);
+				thrown.transform.position = gameObject.transform.position;
 
-                thrown.GetComponentInChildren<Rigidbody2D>().AddForce( dir * forceFactor, ForceMode2D.Impulse );
-                power_newton = 0;
-                time = 0;
-                player.SetState( Player.State.Idle );
-                isUsing = false;
-                GetComponent<Fading>().FadeOut();
+				thrown.GetComponentInChildren<Rigidbody2D>().AddForce( dir * forceFactor, ForceMode2D.Impulse );
+				power_newton = 0;
+				time = 0;
+				player.SetState( Player.State.Idle );
+				isUsing = false;
+				GetComponent<Fading>().FadeOut();
 				GetComponent<PlaySoundEffect>().PlaySoundAtPos();
-            }
-            else
-            {
-                // Aim
-                dir = power_newton * new Vector2( Mathf.Cos( angle_rad ), Mathf.Sin( angle_rad ) );
-                targetSprite.transform.position = gameObject.transform.position + new Vector3( dir.x, dir.y ) / 5;
-                power_newton = Mathf.Sin( time * Mathf.PI / 2 );
-            }
+				myPriest.GetComponent<Animator>().SetBool("Throw", true);
+			}
+			else
+			{
+				// Aim
+				dir = power_newton * new Vector2( Mathf.Cos( angle_rad ), Mathf.Sin( angle_rad ) );
+				targetSprite.transform.position = gameObject.transform.position + new Vector3( dir.x, dir.y ) / 5;
+				power_newton = Mathf.Sin( time * Mathf.PI / 2 );
+			}
 
-            float x = player.Get( AxisAction.AimX );
-            float y = player.Get( AxisAction.AimY );
+			float x = player.Get( AxisAction.AimX );
+			float y = player.Get( AxisAction.AimY );
 
-            angle_rad = Mathf.Atan2( y, x );
-        }
-    }
+			angle_rad = Mathf.Atan2( y, x );
+		}
+	}
 }
